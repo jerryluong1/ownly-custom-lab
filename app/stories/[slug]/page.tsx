cdd788909688504b677ba714d3961d349e26e795
JerryLuong <jerryluong.hau@gmail.com>
2026-06-13 05:07:44 +0700

    Add new slugs for static params in page.tsx

--- app/stories/[slug]/page.tsx
+++ app/stories/[slug]/page.tsx
@@ -3,6 +3,10 @@ import ClientPage from "./ClientPage";
 export function generateStaticParams() {
   return [
     { slug: "11-hang-gai" },
+    { slug: "noi-nang-ve-xuong-che-tac" },
+    { slug: "viet-ve-kinh-mat-ha-noi" },
+    { slug: "nghe-thuat-danh-bong-acetate" },
+    { slug: "tu-ban-ve-den-gong-kinh" },
   ];
 }
 
